6e01c79ace7ba83bcce7e03f901ff2d35549ee46
bewpage <b.jedlinski@bewpage.com>
2019-01-11 20:34:35 +0100

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,33 @@
-//Konfiguracja Webpack
+const path = require('path');
+const glob = require('glob');
 
 module.exports = {
-    entry: "./js/app.jsx",
-    output: { filename: "./js/out.js" },
+    mode: 'development',
+    entry: {
+        filename: glob.sync('js/content'),
+        path: path.resolve(__dirname, 'js')
+    },
+    devtool: 'inline-source-map',
     devServer: {
         inline: true,
         contentBase: './',
         port: 3001
     },
+    output: {
+        filename: "./build/out.js",
+        path: path.resolve(__dirname, 'js')
+    },
     watch: true,
     module: {
-        loaders: [
+        rules: [
             {
-                test: /\.jsx$/, exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: { presets: ['es2015', 'stage-2', 'react'] }
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
             }
         ]
     }
-}
+};
 
